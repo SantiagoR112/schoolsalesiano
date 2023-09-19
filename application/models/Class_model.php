ce6cc8c29ada929d9b2f042e5a0da5fbd4dadeef
SantiagoR112 <u20191177634@usco.edu.co>
2023-09-19 11:47:22 -0500

    Validacion clase y asignatura
    Agregar y actualizar clase y asignatura

--- application/models/Class_model.php
+++ application/models/Class_model.php
@@ -11,28 +11,97 @@ class   Class_model extends CI_Model {
 
 
     // The function below insert into class table //
-    function createClassFunction(){
-
-        $page_data = array(
-            'name' => $this->input->post('name'),
-            'name_numeric' => $this->input->post('name_numeric'),
-            'teacher_id' => $this->input->post('teacher_id')
-			);
-
-        $this->db->insert('class', $page_data);
+    function createClassFunction() {
+        $name = $this->input->post('name');
+        $name_numeric = $this->input->post('name_numeric');
+        $teacher_id = $this->input->post('teacher_id');
+    
+        // Verificar si el docente ya tiene asignada una clase
+        $existing_class_for_teacher = $this->db->get_where('class', array('teacher_id' => $teacher_id))->row();
+    
+        if ($existing_class_for_teacher) {
+            // Si el docente ya tiene asignada una clase, muestra un mensaje de error
+            $this->session->set_flashdata('error_message', 'El docente ya es director de grupo de otra clase.');
+            redirect(base_url() . 'admin/classes', 'refresh'); // Reemplaza 'ruta_de_redireccion' con la URL a la que deseas redirigir
+        } else {
+            // Verificar si ya existe una clase con el mismo nombre o nombre numérico
+            $existing_class_name = $this->db->get_where('class', array('name' => $name))->row();
+            $existing_class_numericname = $this->db->get_where('class', array('name_numeric' => $name_numeric))->row();
+    
+            if ($existing_class_name) {
+                // Si ya existe una clase con el mismo nombre, muestra un mensaje de error
+                $this->session->set_flashdata('error_message', 'Ya existe una clase con el mismo nombre.');
+                redirect(base_url() . 'admin/classes', 'refresh'); // Reemplaza 'ruta_de_redireccion' con la URL a la que deseas redirigir
+            } elseif ($existing_class_numericname) {
+                // Si ya existe una clase con el mismo nombre numérico, muestra un mensaje de error
+                $this->session->set_flashdata('error_message', 'Ya existe una clase con el mismo nombre numérico.');
+                redirect(base_url() . 'admin/classes', 'refresh'); // Reemplaza 'ruta_de_redireccion' con la URL a la que deseas redirigir
+            } else {
+                // Si no existe una clase con el mismo nombre ni con el mismo nombre numérico y el docente no tiene asignada ninguna clase, procede a insertar la nueva clase
+                $page_data = array(
+                    'name' => $name,
+                    'name_numeric' => $name_numeric,
+                    'teacher_id' => $teacher_id
+                );
+    
+                $this->db->insert('class', $page_data);
+            }
+        }
     }
+    
+    
 
-// The function below update class table //
-    function updateClassFunction($param2){
+    // The function below update class table //
+    function updateClassFunction($param2) {
+        $new_name = $this->input->post('name');
+        $new_name_numeric = $this->input->post('name_numeric');
+        $teacher_id = $this->input->post('teacher_id');
+
+        // Obtén los datos actuales de la clase
+        $current_class = $this->db->get_where('class', array('class_id' => $param2))->row();
+
+        // Verifica si los nuevos valores son diferentes de los valores actuales
+        if ($new_name !== $current_class->name || $new_name_numeric !== $current_class->name_numeric) {
+            // Verifica si ya existe otra clase con el mismo name
+            $existing_name = $this->db->get_where('class', array('name' => $new_name))->row();
+            if ($existing_name) {
+                // Si ya existe una clase con el mismo name, muestra un mensaje de error
+                $this->session->set_flashdata('error_message', 'Ya existe una clase con el mismo nombre.');
+                redirect(base_url(). 'admin/classes', 'refresh'); // Reemplaza 'ruta_de_redireccion' con la URL a la que deseas redirigir
+                return; // Detiene la ejecución para evitar la actualización
+            }
+
+            // Verifica si ya existe otra clase con el mismo name_numeric
+            $existing_name_numeric = $this->db->get_where('class', array('name_numeric' => $new_name_numeric))->row();
+            if ($existing_name_numeric) {
+                // Si ya existe una clase con el mismo name_numeric, muestra un mensaje de error
+                $this->session->set_flashdata('error_message', 'Ya existe una clase con el mismo nombre numérico.');
+                redirect(base_url(). 'admin/classes', 'refresh'); // Reemplaza 'ruta_de_redireccion' con la URL a la que deseas redirigir
+                return; // Detiene la ejecución para evitar la actualización
+            }
+        }
+
+        // Verifica si el docente ya tiene asignada otra clase con un teacher_id diferente
+        $existing_teacher_class = $this->db->get_where('class', array('teacher_id' => $teacher_id))->row();
+        if ($existing_teacher_class && $existing_teacher_class->class_id !== $param2) {
+            // Si el docente ya tiene asignada otra clase con teacher_id diferente, muestra un mensaje de error
+            $this->session->set_flashdata('error_message', 'El docente ya es director de grupo de otra clase.');
+            redirect(base_url(). 'admin/classes', 'refresh'); // Reemplaza 'ruta_de_redireccion' con la URL a la que deseas redirigir
+            return; // Detiene la ejecución para evitar la actualización
+        }
+
+        // Si no hay duplicados, el docente no tiene otra clase con teacher_id diferente y los valores no han cambiado, procede con la actualización de la clase
         $page_data = array(
-            'name' => $this->input->post('name'),
-            'name_numeric' => $this->input->post('name_numeric'),
-            'teacher_id' => $this->input->post('teacher_id')
-			);
+            'name' => $new_name,
+            'name_numeric' => $new_name_numeric,
+            'teacher_id' => $teacher_id
+        );
 
         $this->db->where('class_id', $param2);
         $this->db->update('class', $page_data);
     }
+
+
 
     // The function below delete from class table //
     function deleteClassFunction($param2){
--- application/models/Subject_model.php
+++ application/models/Subject_model.php
@@ -10,16 +10,30 @@ class Subject_model extends CI_Model {
 
 
     // The function below insert into subject table //
-    function createSubjectFunction(){
-
-        $page_data = array(
-            'name'          => html_escape($this->input->post('name')),
-            'class_id'      => html_escape($this->input->post('class_id')),
-            'teacher_id'    => html_escape($this->input->post('teacher_id'))
-	    );
-
-        $this->db->insert('subject', $page_data);
+    function createSubjectFunction() {
+        $name = html_escape($this->input->post('name'));
+        $class_id = html_escape($this->input->post('class_id'));
+        $teacher_id = html_escape($this->input->post('teacher_id'));
+    
+        // Verificar si ya existe una materia con el mismo nombre y class_id
+        $existing_subject = $this->db->get_where('subject', array('name' => $name, 'class_id' => $class_id))->row();
+    
+        if ($existing_subject) {
+            // Si ya existe una materia con el mismo nombre y class_id, muestra un mensaje de error
+            $this->session->set_flashdata('error_message', 'Ya existe una asignatura con el mismo nombre para esta clase.');
+            redirect(base_url() . 'subject/subject', 'refresh'); // Reemplaza 'ruta_de_redireccion' con la URL a la que deseas redirigir
+        } else {
+            // Si no existe, procede a insertar la nueva materia
+            $page_data = array(
+                'name' => $name,
+                'class_id' => $class_id,
+                'teacher_id' => $teacher_id
+            );
+    
+            $this->db->insert('subject', $page_data);
+        }
     }
+    
 
 // The function below update subject table //
     function updateSubjectFunction($param2){
