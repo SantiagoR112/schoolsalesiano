020fa58105219bcf0f641d0579b4d5c2d0ad07d7
SantiagoR112 <u20191177634@usco.edu.co>
2023-09-25 22:45:18 -0500

    Barra de busqueda header

--- application/views/backend/header.php
+++ application/views/backend/header.php
@@ -4,10 +4,6 @@
                 <div class="top-left-part"><a class="logo" href="#"><b><img src="<?php echo base_url();?>uploads/logo.png" width="50" height="50" alt="home" /></b><span class="hidden-xs"><strong>COLEGIO</strong>SALESIANO</span></a></div>
                     <ul class="nav navbar-top-links navbar-left hidden-xs">
                         <li><a href="javascript:void(0)" class="open-close hidden-xs waves-effect waves-light"><i class="icon-arrow-left-circle ti-menu"></i></a></li>
-                        <li>
-                            <form role="search" class="app-search hidden-xs">
-                            <input type="text" placeholder="Search..." class="form-control"> <a href=""><i class="fa fa-search"></i></a> </form>
-                        </li>
                     </ul>
             <ul class="nav navbar-top-links navbar-right pull-right">
                 <!--<li class="dropdown"> 
